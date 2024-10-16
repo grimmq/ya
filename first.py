6ea7de2e41b0bcaa6761c0b80b00407788fab85e
rain <grimmq1921@gmail.com>
2024-10-17 00:39:50 +0800

    sub ok

--- first.py
+++ first.py
@@ -1,11 +1,9 @@
 print("我測試看看")
-<<<<<<< HEAD
 
 
 '''
 hotfixed
 '''
-=======
 print("我是分主的主線 我是 feature")
 
 
@@ -20,4 +18,3 @@ print("我是分主的主線 我是 feature")
 
 sub02 ok
 """
->>>>>>> 96003ba (sub2 ok)
